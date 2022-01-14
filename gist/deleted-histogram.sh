67a043768238fbcb566191ce3120075691e8af8c
Zdenek Sustr <git@sustr.net>
2022-01-14 10:08:59 +0100

    Display all deleted for now

--- gist/deleted-histogram.sh
+++ gist/deleted-histogram.sh
@@ -17,7 +17,7 @@
 	let COUNT=$PAGESIZE+1
 	while [ $COUNT -gt $PAGESIZE ]; do
 		COUNT=0
-		SEG=`curl -sS $UPWD ${URL}/odata/v1/DeletedProducts?%24format=text/csv\&%24select=Name,CreationDate,DeletionDate,DeletionCause\&%24skip=$SKIP\&%24top=$PAGESIZE\&%24filter=CreationDate%20gt%20datetime%27${SSTRING}%27%20and%20CreationDate%20lt%20datetime%27${ESTRING}%27%20and%20startswith%28DeletionCause,%27Invalid%27%29`
+		SEG=`curl -sS $UPWD ${URL}/odata/v1/DeletedProducts?%24format=text/csv\&%24select=Name,CreationDate,DeletionDate,DeletionCause\&%24skip=$SKIP\&%24top=$PAGESIZE\&%24filter=CreationDate%20gt%20datetime%27${SSTRING}%27%20and%20CreationDate%20lt%20datetime%27${ESTRING}%27`
 		while read -r line; do
 			if [ $COUNT -ne 0 ]; then
 				echo $line;
